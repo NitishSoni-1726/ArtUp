26c204a610db8df004669a3a189c22b31ffd2ad2
Nitish Soni <nitishsoni890@gmail.com>
2023-02-05 14:08:57 +0530

    h

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const toggleButton = document.getElementById("toggle-button");
 const toggleList = document.getElementById("toggle-menu");
-toggleButton.addEventListener("click", Click);
-function Click() {
-  toggleList.classList.toggle("toggle-active");
+toggleButton.addEventListener("click", navClick);
+function navClick() {
+  toggleList.classList.toggle("displayblock");
+}
+
+const raiseCapital = document.getElementById("raisecapital");
+raiseCapital.addEventListener("click", raiseCapitalClick);
+
+function raiseCapitalClick() {
+  location.href = "https://www.google.com";
+}
+const home = document.getElementById("home");
+home.addEventListener("click", homeclick);
+function homeclick() {
+  location.href = "index.html";
 }
